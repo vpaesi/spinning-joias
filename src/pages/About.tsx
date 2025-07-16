e069016ca29005753c87c7f9072a09eed24853d9
vpaesi <paesivitoria@gmail.com>
2025-07-16 11:30:19 -0300

    feat: atualiza as páginas about e FAQ, e a pasta de assets

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,66 @@
 function About() {
   return (
-    <div>
-      <h1>Sobre Nós</h1>
-      <p>Bem-vindo à nossa loja de joias!</p>
-      <p>Oferecemos uma seleção exclusiva de joias artesanais.</p>
-      <p>Nosso compromisso é com a qualidade e o design único.</p>
+    <div className="flex justify-center items-start min-h-screen bg-yellow-100 py-8 px-4">
+      <div className="w-full max-w-3xl bg-white rounded-lg shadow-lg p-6 md:p-10">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Sobre a Spinning Joias
+        </h1>
+        <div className="space-y-4">
+            <div className="border-b pb-2">
+                            <h2 className="text-2xl font-semibold mt-4 mb-2">Nossa História</h2>
+
+              <p>A Spinning Joias acompanha os mais diversos eventos de patinação <b>desde 2012</b>. Desde o início, nosso objetivo foi criar peças que representassem a elegância e a leveza dos movimentos da patinação.
+              </p>
+              <p>
+                Acreditamos que cada peça de joia deve contar uma história e
+                refletir a personalidade de quem a usa. Por isso, nossa equipe
+                está sempre em busca de inovações e tendências para trazer o
+                melhor em design e qualidade.
+              </p>
+              <p>
+                Com o tempo, <b>expandimos nossa linha de produtos</b> para incluir semijoias que atendem a todos os gostos e estilos, e diferentes produtos para usar fora da quadra de patinação, como bonés, bolsas, viseiras, etc., sempre com a qualidade e o design que nos tornaram conhecidos.
+              </p>
+              <p>
+                A Spinning Joias é mais do que uma marca, é uma comunidade de
+                pessoas com estilo e <b>apaixonadas por patinação</b>. Estamos aqui para oferecer a melhor experiência de compra e atendimento, sempre com um sorriso no rosto e disposição para ajudar.
+              </p>
+              <p>
+                Hoje, somos uma marca reconhecida e com clientes satisfeitos em todo o Brasil. Continuamos a inovar e a buscar novas formas de encantar nossos clientes, sempre com o compromisso de oferecer o melhor em qualidade e atendimento.
+              </p>
+            </div>
+            <div className="border-b pb-2">
+              <h2 className="text-2xl font-semibold mt-4 mb-2">🎯 Missão</h2>
+              <p>
+                Encantar nossos clientes com joias de qualidade, design marcante e preço justo, por meio de uma plataforma moderna, segura e pensada para quem valoriza estilo, praticidade e beleza.
+              </p>
+              </div>
+            <div className="border-b pb-2">
+              <h2 className="text-2xl font-semibold mt-4 mb-2">🌟 Visão</h2>
+              <p>Ser referência nacional em joias que unem elegância e autenticidade, oferecendo uma experiência de compra encantadora e confiável.</p>
+            </div>
+            <div className="border-b pb-2">
+              <h2 className="text-2xl font-semibold mt-4 mb-2">💎 Valores</h2>
+              <p>
+                Nossos valores são a base de tudo o que fazemos. Eles nos guiam em cada decisão, desde a criação de nossos produtos até o atendimento ao cliente. Aqui estão os nossos principais valores:
+              </p>
+              <h3 className="text-xl font-semibold mt-4 mb-2">S.P.I.N.</h3>
+              <ul className="list-disc pl-6 space-y-2">
+                <li>
+                  <b>S</b>ensibilidade: Valorizamos a beleza e a emoção que cada joia ou produto pode transmitir, criando peças que tocam o coração.
+                </li>
+                <li>
+                  <b>P</b>recisão: Cada detalhe conta. Buscamos a perfeição em cada peça, garantindo qualidade e durabilidade.
+                </li>
+                <li>
+                  <b>I</b>novação: Estamos sempre em movimento, buscando novas tendências e tecnologias para surpreender nossos clientes.
+                </li>
+                <li>
+                  <b>N</b>obreza: Valorizamos a autenticidade e a singularidade de cada cliente, promovendo respeito e ética em todas as nossas ações.
+                </li>
+              </ul>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -1,23 +1,242 @@
+import modal_encomende from "../assets/modal_encomende.png";
+import diferenca_semijoia from "../assets/diferenca_semijoia.jpg";
+import personalInfo from "../utils/DadosSpinning";
+import { useState } from "react";
+
 function FAQ() {
+  const tabelaDeFrete = [
+    {
+      regiao: "Sul",
+      valor: 15.0,
+    },
+    {
+      regiao: "Sudeste",
+      valor: 25.0,
+    },
+    {
+      regiao: "Centro-Oeste",
+      valor: 35.0,
+    },
+    {
+      regiao: "Nordeste",
+      valor: 40.0,
+    },
+    {
+      regiao: "Norte",
+      valor: 50.0,
+    },
+  ];
+
+  const perguntas = [
+    {
+      pergunta: "Como comprar online?",
+      resposta: (
+        <>
+          <p>
+            Por ora, o site da Spinning funciona como um <b>catálogo</b>. O que
+            isso significa?
+          </p>
+          <p>
+            No site está catalogada toda a coleção da Spinning Joias, dessa
+            forma, você pode decidir quais produtos deseja e entrar em contato
+            conosco através do WhatsApp (clicando{" "}
+            <a
+              href={`${personalInfo.socialMedia.whats}`}
+              className="text-blue-600 underline"
+            >
+              aqui
+            </a>
+            ) ou Instagram (clicando{" "}
+            <a
+              href={`${personalInfo.socialMedia.instagram}`}
+              className="text-blue-600 underline"
+            >
+              aqui
+            </a>
+            ) para fazer o pedido.
+          </p>
+          <p>
+            Para facilitar esse processo, na página de cada produto há um botão
+            que redireciona direto para o Whats, veja:
+          </p>
+          <img
+            src={modal_encomende}
+            alt="Modal de encomenda"
+            className="w-full h-auto mt-4 rounded-lg shadow-lg"
+          />
+        </>
+      ),
+    },
+    {
+      pergunta: "Como comprar presencialmente?",
+      resposta: (
+        <>
+          <p>
+            A Spinning Joias participa dos principais{" "}
+            <b>eventos de patinação artística do Brasil</b>, onde você pode
+            encontrar nossa stand com semijoias e demais produtos relacionados à
+            patinação.
+          </p>
+          <p>
+            Fique atento(a) ao nosso{" "}
+            <a
+              href={`${personalInfo.socialMedia.instagram}`}
+              className="text-blue-600 underline"
+            >
+              Instagram
+            </a>{" "}
+            para saber onde estaremos! Lembramos também que para garantir a sua
+            peça, você pode reservar pelo WhatsApp ou Instagram, e retirar no
+            evento.
+          </p>
+        </>
+      ),
+    },
+    {
+      pergunta: "Quais são as formas de pagamento aceitas?",
+      resposta: (
+        <>
+          <p>
+            <b>Pix/transferência</b>: Para compras online (encomendas realizadas
+            pelo{" "}
+            <a
+              href={`${personalInfo.socialMedia.whats}`}
+              className="text-blue-600 underline"
+            >
+              Whats
+            </a>{" "}
+            ou pelo{" "}
+            <a
+              href={`${personalInfo.socialMedia.instagram}`}
+              className="text-blue-600 underline"
+            >
+              Instagram
+            </a>
+            ).{" "}
+          </p>
+          <p>
+            <b>Cartão de crédito/débito</b>: Para compras presenciais em
+            eventos.
+          </p>
+        </>
+      ),
+    },
+    {
+      pergunta: "Qual é o prazo e valor de entrega?",
+      resposta: (
+        <>
+          <p>
+            O prazo e o valor de entrega{" "}
+            <b>varia de acordo com a sua localização.</b>
+          </p>
+          <p>
+            A entrega, geralmente, leva entre 5 a 10 dias úteis e tem um valor
+            aproximado ao da tabela abaixo, veja:
+          </p>
+          <div className="overflow-x-auto mt-4">
+            <table className="min-w-full border border-gray-300 text-center">
+              <thead className="bg-yellow-200">
+                <tr>
+                  <th className="border px-2 py-1">Região¹</th>
+                  <th className="border px-2 py-1">Valor do Frete (R$)²</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tabelaDeFrete.map((row, i) => (
+                  <tr key={i} className="odd:bg-yellow-50">
+                    <td className="border px-2 py-1">{row.regiao}</td>
+                    <td className="border px-2 py-1">{row.valor.toFixed(2)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <p className="mt-4 text-sm text-gray-500">
+            ¹ Região de destino, ou seja, a sua localização.
+          </p>
+          <p className="mt-2 text-sm text-gray-500">
+            ² Valores aproximados, podendo variar de acordo com o peso e
+            dimensões do produto, considerando o envio via Correios com origem
+            de Porto Alegre/RS.
+          </p>
+        </>
+      ),
+    },
+    {
+      pergunta: "Os produtos são de ouro/prata ou bijuteria?",
+      resposta: (
+        <>
+          <p>
+            <b>A Spinning trabalha com semijoia</b>. O que isso significa?
+          </p>
+          <p>
+            As semijoias da Spinning são feitas com metais nobres, como{" "}
+            <b>prata 925</b> ou banhadas a <b>ouro 18k</b>.{" "}
+          </p>
+          <p>
+            De forma que, possuem um banho de alta qualidade que garante
+            durabilidade e resistência, além de, possuirem antialérgico.
+          </p>
+          <img
+            src={diferenca_semijoia}
+            alt="Diferença entre joia, semijoia  bijuteria"
+            className="w-full h-auto mt-4 rounded-lg shadow-lg"
+          />
+        </>
+      ),
+    },
+    {
+      pergunta: "Como posso cuidar das minhas semijoias?",
+      resposta: (
+        <p>
+          Para garantir a durabilidade das suas semijoias, evite o contato com
+          produtos químicos, como perfumes e produtos de limpeza. Guarde-as em
+          locais secos e arejados, longe da luz direta do sol.
+        </p>
+      ),
+    },
+    {
+      pergunta: "As semijoias tem garantia?",
+      resposta: (
+        <p>Sim, todas as nossas joias tem garantia do banho e antialérgico.</p>
+      ),
+    },
+  ];
+
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  const handleToggle = (idx: number) => {
+    setOpenIndex(openIndex === idx ? null : idx);
+  };
+
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Perguntas Frequentes (FAQ)</h1>
-      <div className="space-y-6">
-        <div>
-          <h2 className="text-xl font-semibold mb-2">Como posso fazer um pedido?</h2>
-          <p>Você pode fazer um pedido diretamente pelo nosso site, selecionando os produtos desejados e adicionando-os ao carrinho.</p>
-        </div>
-        <div>
-          <h2 className="text-xl font-semibold mb-2">Quais são as formas de pagamento aceitas?</h2>
-          <p>Aceitamos cartões de crédito, débito e pagamentos via Pix.</p>
-        </div>
-        <div>
-          <h2 className="text-xl font-semibold mb-2">Qual é o prazo de entrega?</h2>
-          <p>O prazo de entrega varia de acordo com a sua localização. Geralmente, leva entre 5 a 10 dias úteis.</p>
-        </div>
-        <div>
-          <h2 className="text-xl font-semibold mb-2">Posso trocar ou devolver um produto?</h2>
-          <p>Sim, você pode trocar ou devolver um produto em até 30 dias após o recebimento, desde que esteja em perfeitas condições.</p>
+    <div className="flex justify-center items-start min-h-screen bg-yellow-100 py-8 px-4">
+      <div className="w-full max-w-3xl bg-white rounded-lg shadow-lg p-6 md:p-10">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Perguntas Frequentes (FAQ)
+        </h1>
+        <div className="space-y-4">
+          {perguntas.map((faq, idx) => (
+            <div key={idx} className="border-b pb-2">
+              <button
+                className="flex justify-between items-center w-full text-left text-xl font-semibold mb-2 focus:outline-none"
+                onClick={() => handleToggle(idx)}
+                aria-expanded={openIndex === idx}
+                aria-controls={`faq-panel-${idx}`}
+              >
+                <span>{faq.pergunta}</span>
+                <span className="ml-2">{openIndex === idx ? "▲" : "▼"}</span>
+              </button>
+              {openIndex === idx && (
+                <div
+                  id={`faq-panel-${idx}`}
+                  className="pl-2 md:pl-4 animate-fadeIn"
+                >
+                  {faq.resposta}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
